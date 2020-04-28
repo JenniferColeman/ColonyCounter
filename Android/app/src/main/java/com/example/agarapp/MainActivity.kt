65774fecce04c6a4d7b342f1a5e4135eab71cfaf
Hanger, Derek T.(S&T-Student) <dth5h8@mst.edu>
2020-04-28 16:52:25 -0500

    added Volley to toolbox

--- Android/app/src/main/java/com/example/agarapp/MainActivity.kt
+++ Android/app/src/main/java/com/example/agarapp/MainActivity.kt
@@ -8,8 +8,14 @@ import android.view.MenuItem
 import android.content.Intent
 import android.graphics.Bitmap
 import android.provider.MediaStore
+import android.widget.TextView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 import kotlinx.android.synthetic.main.activity_main.*
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,6 +30,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         takePhoto.setOnClickListener {
+            dispatchHandleRequest();
             dispatchTakePictureIntent();
         }
 
@@ -31,6 +38,27 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, ResultActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun dispatchHandleRequest() {
+        val textView = findViewById<TextView>(R.id.text)
+// ...
+
+// Instantiate the RequestQueue.
+        val queue = Volley.newRequestQueue(this)
+        val url = "http://localhost:5000/"
+
+// Request a string response from the provided URL.
+        val stringRequest = StringRequest(
+            Request.Method.GET, url,
+            Response.Listener<String> { response ->
+                // Display the first 500 characters of the response string.
+                textView.text = "Response is: ${response.substring(0, 500)}"
+            },
+            Response.ErrorListener { textView.text = "That didn't work!" })
+
+// Add the request to the RequestQueue
+        queue.add(stringRequest)
     }
 
     private fun pickImageFromGallery() {
---
+++ Python/app/db_entry.py
@@ -0,0 +1,5 @@
+
+class AgarPlate:
+    def __init__(self, img):
+        self.img = img
+        self.date = datetime.today().strftime('%Y-%m-%d-%H:%M:%S')
